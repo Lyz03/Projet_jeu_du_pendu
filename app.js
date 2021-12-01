7319cf08562763a97eba328e488859ed81ce0798
Lyz03 <lizoe@netcourrier.com>
2021-12-01 11:45:49 +0100

    continu js

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
-let user = {
-    letter: document.getElementById('letter'),
+const User = {
+    input: document.getElementById('letter'),
     submit: document.getElementById('submit')
 }
 
-let wordsToFind = ['test', 'autreTest'];
-let searchedWord;
+const Words = {
+    wordsToFind: ['test', 'autreTest'],
 
-console.log(wordsToFind);
-
-function randomWords() {
-    const randomNumber = Math.floor(Math.random() * wordsToFind.length);
-    return wordsToFind[randomNumber];
+    randomWord: function () {
+    const randomNumber = Math.floor(Math.random() * this.wordsToFind.length);
+    return this.wordsToFind[randomNumber];
+    }
 }
 
-searchedWord = randomWords();
-console.log(searchedWord)
+let searchWord = Words.randomWord();
+let letter;
+let goodLetter = false;
+
+User.submit.addEventListener("click", function () {
+    if (User.input.value.length === 1) {
+        letter = User.input.value;
+        User.input.value = '';
+        letterInWord();
+        console.log(goodLetter);
+    }
+});
+
+console.log(searchWord);
+function letterInWord() {
+    if (searchWord.includes(letter)) {
+        goodLetter = true;
+    }
+}
