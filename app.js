2fea8414ce687d0f4cacfd18c4a4da77e33142c6
Lyz03 <lizoe@netcourrier.com>
2021-12-01 16:41:19 +0100

    continu js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const User = {
     input: document.getElementById('letter'),
-    submit: document.getElementById('submit')
+    submit: document.getElementById('submit'),
+    userGuesses: document.getElementById('user_guesses')
 }
 
 const Words = {
@@ -12,22 +13,38 @@ const Words = {
     }
 }
 
-let searchWord = Words.randomWord();
+let searchWord = Words.randomWord().split('');
 let letter;
 let goodLetter = false;
+let alreadyWrite = [];
 
+/*
+ * what to do with the user guesses
+ */
 User.submit.addEventListener("click", function () {
     if (User.input.value.length === 1) {
         letter = User.input.value;
-        User.input.value = '';
-        letterInWord();
-        console.log(goodLetter);
+        if (alreadyWrite.includes(letter)) {
+            console.log('deja utiliser')
+        } else {
+            User.input.value = '';
+            letterInWord();
+            console.log(goodLetter);
+            console.log(alreadyWrite);
+        }
+        alreadyWrite.push(letter);
     }
 });
 
 console.log(searchWord);
+
+/*
+ * if the word include the letter then set goodLetter to true
+ */
 function letterInWord() {
-    if (searchWord.includes(letter)) {
-        goodLetter = true;
+    goodLetter = searchWord.includes(letter);
+
+    if (goodLetter) {
+
     }
 }
